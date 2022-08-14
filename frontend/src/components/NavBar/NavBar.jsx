42f6fd44a156cb3ec859467f941db87736296219
Kendall Morgan <kuyukip@gmail.com>
2022-08-14 15:30:47 -0500

    work on navbar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -7,21 +7,30 @@ import "./NavBar.css";
 const Navbar = () => {
   const { logoutUser, user } = useContext(AuthContext);
   const navigate = useNavigate();
+
   return (
     <div className="navBar">
       <ul>
         <li className="brand">
           <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            <b>React/Django JWT</b>
+            <b>{user.username.toUpperCase()}'S PROFILE</b>
           </Link>
         </li>
-        <li>
-          {user ? (
-            <button onClick={logoutUser}>Logout</button>
-          ) : (
-            <button onClick={() => navigate("/login")}>Login</button>
-          )}
-        </li>
+        <div className="buttons-space">
+          <li className="buttons-2">
+            <button>Albums</button>
+          </li>
+          <li className="buttons-2">
+            <button>Theme</button>
+          </li>
+          <li className="buttons-2">
+            {user ? (
+              <button onClick={logoutUser}>Logout</button>
+            ) : (
+              <button onClick={() => navigate("/login")}>Login</button>
+            )}
+          </li>
+        </div>
       </ul>
     </div>
   );
