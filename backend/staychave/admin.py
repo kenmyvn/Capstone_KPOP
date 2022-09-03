9ecb101f021bb20d0d73ddddb27ee3db432992fe
Kendall Morgan <kuyukip@gmail.com>
2022-09-03 12:00:57 -0500

    register to admin

--- backend/staychave/admin.py
+++ backend/staychave/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import StayCHave
 
 # Register your models here.
+admin.site.register(StayCHave)
--- backend/staycwant/admin.py
+++ backend/staycwant/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import StayCWant
 
 # Register your models here.
+admin.site.register(StayCWant)
