ffc00451de86b50956b9cf24c918ddf1d8abe68e
Kendall Morgan <kuyukip@gmail.com>
2022-09-11 22:16:52 -0500

    update profile page

--- frontend/src/pages/ProfilePage/ProfilePage.js
+++ frontend/src/pages/ProfilePage/ProfilePage.js
@@ -2,11 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import "./ProfilePage.css";
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext";
 
 const ProfilePage = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="container">
-      <h1 className="profileheading">hello</h1>
+      <h1 className="profileheading">
+        {user ? user.username : "not logged in"}'s profile page
+      </h1>
       <div className="buttonholder">
         <div className="profilebutton">
           <Link to={`/owned`}>
